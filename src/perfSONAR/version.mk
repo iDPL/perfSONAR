db8570dc71bbf7d67283fe0f41d90aee134edb83
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2014-07-23 09:58:44 -0700

    Testing build on 3.4rc2

--- src/perfSONAR/version.mk
+++ src/perfSONAR/version.mk
@@ -1,3 +1,11 @@
 NAME=perfSONAR
+# Production Version
 VERSION=3.3.2
+I2_REPO_VER=0.4-2
+
+# Release Candidate Version
+VERSION=3.4.rc2
+I2_REPO_VER=0.5-2
+
+#
 RELEASE=0
--- version.mk
+++ version.mk
@@ -1,5 +1,8 @@
 ROLLNAME	= perfSONAR
+# Production Version
 VERSION		= 3.3.2
+# Release Candidate Version
+VERSION		= 3.4rc2
 RELEASE		= 0
 COLOR		= skyblue
 
